361927a80000018291dbc262716063fd15af242c
Abhijith232373 <abhijithpr366@gmail.com>
2025-11-01 12:54:45 +0530

    learn shot decleration :=

---
+++ Ass-3/ass3.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	messageStart:="Happy birthday!"
+	age:=21
+	messageEnd:="years old!"
+	fmt.Printf("%s You are now %d %s",messageStart, age, messageEnd)
+}
